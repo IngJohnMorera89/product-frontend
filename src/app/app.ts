2034cd9876baa4c808a642c725206597ba625ad5
Ing John Morera Barrera <johnmorera.89@gmail.com>
2026-03-10 22:25:34 -0500

    clase 6

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,9 +1,10 @@
 import { Component, inject } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { Header } from './features/shared/header/header';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, Header],
   templateUrl: './app.html',
   styleUrl: './app.css',
 })
--- src/app/features/products/components/product-list/product-list.ts
+++ src/app/features/products/components/product-list/product-list.ts
@@ -1,4 +1,4 @@
-import { Component, inject, OnInit, signal } from '@angular/core';
+import { Component, computed, inject, OnInit, signal } from '@angular/core';
 import { Product } from '../../models/product';
 import { CurrencyPipe, DecimalPipe } from '@angular/common';
 import { ProductService } from '../../services/ProductService';
@@ -14,18 +14,30 @@ export class ProductList implements OnInit {
   private readonly productService = inject(ProductService);
   private readonly router = inject(Router);
 
+  protected readonly searchText = signal('');
+
+  private readonly allProducts = signal<Product[]>([]);
+  protected readonly products = computed(() => {
+    if (this.searchText() === '') {
+      return this.allProducts();
+    } else {
+      return this.allProducts().filter((p) =>
+        p.name.toLowerCase().includes(this.searchText().toLowerCase()),
+      );
+    }
+  });
+
   ngOnInit(): void {
     this.loadProducts();
   }
-  protected readonly products = signal<Product[]>([]);
 
   private loadProducts() {
     const list = this.productService.getAllProducts();
-    this.products.set(list);
+    this.allProducts.set(list);
   }
 
-  onSearch() {
-    this.products.set([]);
+  onSearch(searchValue: string) {
+    this.searchText.set(searchValue);
   }
 
   onView(product: Product) {
