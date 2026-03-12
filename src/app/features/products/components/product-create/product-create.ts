1e52caaa316dbfe3005e25d780a5ffb984896194
Ingjohn_Morera <johnnmorera.89@gmail.com>
2026-03-12 11:56:59 -0500

    finish class6

--- src/app/features/products/components/product-create/product-create.ts
+++ src/app/features/products/components/product-create/product-create.ts
@@ -1,11 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-product-create',
-  imports: [],
+  imports: [ReactiveFormsModule],
   templateUrl: './product-create.html',
   styleUrl: './product-create.css',
 })
 export class ProductCreate {
+  private readonly fb = inject(FormBuilder);
+  protected productForm = this.fb.group({
+    code: [0, [Validators.required]],
+    name: ['', [Validators.required, Validators.minLength(3)]],
+    price: [0, [Validators.required, Validators.min(500)]],
+    description: '',
+    imageUrl: ['', [Validators.required]],
+  });
 
+  onSubmit() {
+    alert(JSON.stringify(this.productForm.value));
+  }
 }
